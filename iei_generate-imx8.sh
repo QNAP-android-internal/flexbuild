1a3e34fd75362d73492ecf383d330a72e45c37e7
Wig Cheng <wigcheng@ieiworld.com>
2024-11-11 10:45:38 +0800

    iei_generate-imx8.sh: improve rootfs post-process function
    Add auto acception to apt command.

--- iei_generate-imx8.sh
+++ iei_generate-imx8.sh
@@ -15,7 +15,10 @@ export CROSS_COMPILE=$CC
 bld -m imx8mpevk
 
 # iei added
-sudo LANG=C chroot build_lsdk2406/rfs/rootfs_lsdk2406_debian_desktop_arm64  /bin/bash -c "apt update; apt install modemmanager"
+sudo LANG=C chroot build_lsdk2406/rfs/rootfs_lsdk2406_debian_desktop_arm64  /bin/bash -c " \
+    yes "Y" | apt update; \
+    yes "Y" | apt install modemmanager \
+"
 
 bld packrfs -p IMX
 
